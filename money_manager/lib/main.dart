bda112622056403062867f8f68fcdabfda7503d5
Ender <ndxendernight@gmail.com>
2024-02-22 17:08:25 +0100

    App scaffold, hello world

--- money_manager/lib/main.dart
+++ money_manager/lib/main.dart
@@ -1,3 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:money_manager/page/home_page.dart';
+
 void main() {
-  print('Hello World!');
+  runApp(const MoneyManagerApp());
+}
+
+class MoneyManagerApp extends StatelessWidget {
+  const MoneyManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
 }
