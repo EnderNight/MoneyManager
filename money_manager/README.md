71391e67fff3ba90e1ec4d2a422919fade745d4e
Ender <ndxendernight@gmail.com>
2024-07-09 23:39:27 +0200

    Added flutter project

---
+++ money_manager/README.md
@@ -0,0 +1,3 @@
+# money_manager
+
+A new Flutter project.
---
+++ money_manager/android/app/src/main/kotlin/com/example/money_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.money_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
