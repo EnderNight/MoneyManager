f94f95ddf69b242f57e3206044413e4582ac786f
Ender <ndxendernight@gmail.com>
2024-07-26 19:29:56 +0200

    Moving to clean architecture

--- money_manager/lib/main.dart
+++ money_manager/lib/main.dart
@@ -1,13 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
-import 'package:money_manager/app.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Hive.initFlutter();
-
-  await Hive.openBox('expenses');
-
-  runApp(const MoneyManagerApp());
+void main() {
+  print("Hello World!");
 }
