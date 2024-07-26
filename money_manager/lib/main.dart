3573a6364e42198a461d02404fc9eb0583f349ce
Ender <ndxendernight@gmail.com>
2024-07-26 19:34:03 +0200

    Done hello world

--- money_manager/lib/main.dart
+++ money_manager/lib/main.dart
@@ -1,3 +1,20 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  print("Hello World!");
+  runApp(const MoneyManagerApp());
+}
+
+class MoneyManagerApp extends StatelessWidget {
+  const MoneyManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
 }
