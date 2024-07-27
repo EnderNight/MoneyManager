c86dbf14fcf1c6357c3c6cd5b5f0018bc9cfbec1
Ender <ndxendernight@gmail.com>
2024-07-27 14:16:19 +0200

    Added Hello World

--- money_manager/lib/main.dart
+++ money_manager/lib/main.dart
@@ -12,7 +12,7 @@ class MoneyManagerApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World'),
+          child: Text('Hello World!'),
         ),
       ),
     );
