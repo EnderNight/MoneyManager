16cd579c82070ed5b18a9404ccb73932ce616e52
Ender <ndxendernight@gmail.com>
2024-07-27 18:21:58 +0200

    Moving to clean architecture

--- money_manager/lib/main.dart
+++ money_manager/lib/main.dart
@@ -1,20 +1,3 @@
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MoneyManagerApp());
-}
-
-class MoneyManagerApp extends StatelessWidget {
-  const MoneyManagerApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  print('Hello World');
 }
